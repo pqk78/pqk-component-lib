3d7312f422ed0afd68864be9152653d7fe5fc781
Maz Planchak <maz.planchak@proton.me>
2025-07-08 13:54:19 -0400

    add button component

---
+++ stories/components/button/Button.stories.js
@@ -0,0 +1,56 @@
+import './button.css';
+import { liquid } from '../../shared/engine';
+
+// More on how to set up stories at: https://storybook.js.org/docs/writing-stories
+export default {
+  title: 'Components/Button',
+  tags: ['autodocs'],
+  render: args => liquid('components/button/button', args),
+  argTypes: {
+    label: { control: 'text' },
+    theme: {
+      control: { type: 'select' },
+      options: ['primary', 'secondary', 'reverse'],
+    },
+    onClick: { action: 'onClick' },
+    size: {
+      control: { type: 'select' },
+      options: ['default', 'compact'],
+    },
+  },
+};
+
+export const Primary = {
+  args: {
+    label: 'Button',
+    theme: ''
+  },
+};
+
+export const Secondary = {
+  args: {
+    label: 'Button',
+    theme: 'secondary',
+  },
+};
+
+export const Reverse = {
+  args: {
+    label: 'Button',
+    theme: 'reverse',
+  },
+};
+
+// export const Large = {
+//   args: {
+//     size: 'large',
+//     label: 'Button',
+//   },
+// };
+
+// export const Small = {
+//   args: {
+//     size: 'small',
+//     label: 'Button',
+//   },
+// };
