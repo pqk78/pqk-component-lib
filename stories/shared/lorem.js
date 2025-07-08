af6c744c1b05ecac3a4e1e6ceef4828b9e20b7f1
Maz Planchak <maz.planchak@proton.me>
2025-07-08 17:32:15 -0400

    update lorem source

--- stories/shared/lorem.js
+++ stories/shared/lorem.js
@@ -1,4 +1,4 @@
-export const routerLorem = [
+export default [
   {
     label: 'Canvas Scrape',
     description: 'You really cannot make a mistake here. Scrape that canvas hard!',
@@ -18,22 +18,42 @@ export const routerLorem = [
   {
     label: 'Happy Medium',
     description: 'At some point in between, you have to reach a happy medium.',
-    paragraphs: [],
+    paragraphs: [
+      'A very quiet little sky. Mountains are so simple that they’re hard! At some point in between, you have to reach a happy medium. Now we can start coming forward. We don’t know where that goes.'
+    ],
   },
   {
     label: 'Fun Today',
     description: 'Let’s just have some fun today. There’s no big secret to this.',
-    paragraphs: [],
+    paragraphs: [
+      'Just walk right back into this painting. Let’s have some fun today. Just making little X’s, little criss-cross strokes. Anything you are willing to practice, you can do! We use no patterns, and no tracings. Nothing to it. Let’s have some big, fluffy clouds in this painting.',
+      'Only takes a minute. Now let’s use the fan brush. Shh... our secret! Don’t tell, just enjoy! Always start with a clean brush in a light area and work outward. That blue is so strong, it’ll eat up your whole world in a heartbeat. Learn to control it. Be... so... careful!'
+    ],
+  },
+  {
+    label: 'Be Brave!',
+    description: 'You learn to take advantage of whatever happens and use it to make your painting special.',
+    paragraphs: [
+      'Just enough to give it an upward swirl. Angles are very, very important. Save your detail for the foreground. You know me -- I love water. It’s a fantastic day here!',
+      'Be brave! You learn to take advantage of whatever happens and use it to make your painting special. Make big billowy, fluffy clouds. We’re just looking for a nice, basic, outside shape. We could care less what’s going on inside here. We push quite hard!'
+    ]
   },
   {
     label: 'Out on a Limb',
     description: 'Don’t be afraid to go out on a limb once in a while.',
-    paragraphs: [],
+    paragraphs: [
+      'Anyone can paint! Let’s make this a little more interesting. We’ll blend it all together. Try not to kill it. I like to be able to create my own world.',
+      'These are far away, and I’m not looking for a lot of detail. You have to make all these big decisions when you have power. Just a nice little mountain, a little friendly guy. Just make little criss-cross strokes. Use a color that’s a little more muted and quiet.'
+    ],
   },
   {
     label: 'Dead Color',
     description: 'If you over-mix it, you just get one dead color.',
-    paragraphs: [],
+    paragraphs: [
+      'All you’re worried about is this nice outside shape. Son of a gun, it looks like we’ve got two big eyes on there. If you over-mix it, you just get one dead color. You can continue to blend it until it just goes away. Super light touch here.',
+      'Let’s make some happy little clouds in our world. Just let it go, have fun. That’s the job I want in my next life -- I wanna come back and be a cloud and just float around and have a good time. Just sorta work it around and around here. Just let it sort of run across here, wherever you want it to go.',
+      'This is one that you can do. It doesn’t matter in our world where it goes. Gently, gently, gently blend that together. Let it sort of bounce around and play. You’d better get out your big coat and put it on.'
+    ],
   },
   {
     label: 'Here Comes a Tree',
@@ -43,6 +63,33 @@ export const routerLorem = [
   {
     label: 'Secret',
     description: 'If there’s a secret to anything, it’s practice. This is where the whole ecosystem starts.',
-    paragraphs: [],
+    paragraphs: [
+      'If there’s a secret to anything, it’s practice. It’s easy to add more color, but it’s a son of a gun to take it away. Absolutely no limits! You know me, I get crazy. Follow these angles.',
+      'Now we’ll fluff it. Do a cabin-ectomy. There’s some little stringy ones that live down here. There’s nothing worse than an angry tree. I think you’ll be tickled with what you can do with something that starts out looking this bad.'
+    ],
+  },
+  {
+    label: 'Let’s Get Crazy',
+    description: 'People are so excited when they see this happen. Let’s get crazy.',
+    paragraphs: [
+      'Looks like there’s a storm coming already. Let’s build us a mountain! People are so excited when they see this happen. Let’s get crazy. You can change your mind right mid stream. It’ll open whole new worlds for you once you learn to make friends with this knife. Be careful with this bright red -- it’ll eat up your whole world in a heartbeat.',
+      'That knife’s a little big... let me get my small knife. We’ll make some big ol’ huge mountains! That’ll be a challenge! Let’s just have some fun today. Quickly drop in just a little warm part of the sky here. It’s extremely, extremely effective. You have unlimited power here.'
+    ]
+  },
+  {
+    label: 'A Paper Towel',
+    description: 'You don’t have to tell them you did this with a paper towel.',
+    paragraphs: [
+      'You don’t have to tell them you did this with a paper towel. ’Course I make some mistakes at home, and we call those "abstracts". You need to understand nature. Wind this up and blend it together. We are really trying to teach you a technique. We’re not trying to teach you to copy.',
+    ]
+  },
+  {
+    label: 'Get Tough With It',
+    description: 'Everybody has their own idea of what a mountain should look like.',
+    paragraphs: [
+      'Sometimes it’s a lot of fun to put several layers. Just a nice little mountain, a little friendly guy. Always start with a clean brush in a light area and work outward. Have to have dark in order to show light. Mountains come in all shapes and sizes.',
+      'Get tough with it. Everybody has their own idea of what a mountain should look like. Just let it sort of flow. Another thing that seems to give us a lot of problems is almighty clouds. Life’s wonderful, isn’t it?',
+      'That knife’s a little big... let me get my small knife. We’ll have a super time. Just let it blend up. Shh... our secret! Don’t tell, just enjoy! Today’s such a wonderful day, let’s just make a happy little painting.'
+    ]
   }
 ];
