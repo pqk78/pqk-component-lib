f725b7d33099b453759c39db9c573dfbb684bc60
Maz Planchak <maz.planchak@proton.me>
2025-07-09 17:22:15 -0400

    set jsTruthy to true

--- stories/shared/engine.js
+++ stories/shared/engine.js
@@ -3,6 +3,7 @@ import { Liquid } from 'liquidjs';
 const engine = new Liquid({
   root: ['stories/'],
   extname: '.liquid',
+  jsTruthy: true,
 });
 
 export const liquid = (template, args) => engine.renderFileSync(template, {...args});
