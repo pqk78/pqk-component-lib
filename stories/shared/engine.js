c452d29e9aaaa38bae15e5bdcce95af6e801ad5d
Maz Planchak <maz.planchak@proton.me>
2025-07-12 19:15:51 -0400

    add njk option for rendering

--- stories/shared/engine.js
+++ stories/shared/engine.js
@@ -1,10 +1,18 @@
 import { Liquid } from 'liquidjs';
 import lorem from './lorem';
+import nunjucks from 'nunjucks';
 
-const engine = new Liquid({
-  root: ['stories/'],
-  extname: '.liquid',
-  jsTruthy: true,
-});
+export const njk = (template, args) => {
+  nunjucks.configure('stories', {
+  });
+  return nunjucks.render(template, {...args, lorem, delta: new Date().getMilliseconds()});
+}
 
-export const liquid = (template, args) => engine.renderFileSync(template, {...args, lorem, delta: new Date().getMilliseconds()});
+export const liquid = (template, args) => {
+  const engine = new Liquid({
+    root: ['stories/'],
+    extname: '.liquid',
+    jsTruthy: true,
+  });
+  return engine.renderFileSync(template, {...args, lorem, delta: new Date().getMilliseconds()});
+};
